4b87b30f9bc8941d81ce04cc98fcd0f833fe7fb9
杨冬武 <apple@DWdeiMac.local>
2017-10-18 16:28:16 +0800

    map and gesture

--- DYGestureRecognizerDemo/DYGestureRecognizerDemo/ViewController.m
+++ DYGestureRecognizerDemo/DYGestureRecognizerDemo/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIGestureRecognizerDelegate>
+
+@property (nonatomic, strong) UITapGestureRecognizer *tapGesture;
+@property (nonatomic, strong) UIPanGestureRecognizer *panGesture;
 
 @end
 
@@ -16,9 +19,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapGesture:)];
+    self.tapGesture.delegate = self;
+    self.tapGesture.numberOfTapsRequired = 1;
+    self.tapGesture.numberOfTouchesRequired = 1;
+    self.tapGesture.cancelsTouchesInView = NO;
+    [self.view addGestureRecognizer:self.tapGesture];
+    
+    self.panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture:)];
+    self.panGesture.delegate = self;
+    self.panGesture.minimumNumberOfTouches = 1;
+    self.panGesture.maximumNumberOfTouches = 1;
+    self.panGesture.cancelsTouchesInView = YES;
+    [self.view addGestureRecognizer:self.panGesture];
 }
 
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch {
+    //CGPoint location = [gestureRecognizer locationInView:self.view];
+    NSLog(@"%s\n\n%@\n\n\n", __FUNCTION__,touch);
+    return YES;
+}
+
+- (void)tapGesture:(UITapGestureRecognizer *)gestureRecognizer {
+    CGPoint location = [gestureRecognizer locationInView:self.view];
+    NSLog(@"%@",NSStringFromCGPoint(location));
+}
+
+- (void)panGesture:(UIPanGestureRecognizer *)gestureRecognizer {
+    CGPoint location = [gestureRecognizer locationInView:self.view];
+    NSLog(@"%@",NSStringFromCGPoint(location));
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
