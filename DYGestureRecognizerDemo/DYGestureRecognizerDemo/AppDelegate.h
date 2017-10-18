faa009aaf4f0cd0a26c1427c128979af9a6540b9
杨冬武 <apple@DWdeiMac.local>
2017-10-18 16:07:52 +0800

    init

---
+++ DYGestureRecognizerDemo/DYGestureRecognizerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYGestureRecognizerDemo
+//
+//  Created by Apple on 2017/10/18.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYGestureRecognizerDemo/DYGestureRecognizerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYGestureRecognizerDemo
+//
+//  Created by Apple on 2017/10/18.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
